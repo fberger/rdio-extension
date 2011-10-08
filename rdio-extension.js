248e1f6e57eb83f408fec0401a7a93bd796e96fe
Felix Berger <bflat1@gmx.net>
2011-10-07 21:55:10 -0400

    working

--- rdio-extension.js
+++ rdio-extension.js
@@ -1,4 +1,4 @@
-var debug = false;
+var debug = true;
 function log() {
     if (debug) {
 	console.log.apply(console, arguments);
@@ -16,17 +16,42 @@ function codeToString(f) {
 
 function injectedJs() {
     jQuery.fn.origAutoSuspenders = jQuery.fn.autoSuspenders;
-    jQuery.fn.autoSuspenders = function(j, v) {
-	var result = jQuery.fn.origAutoSuspenders.call(this, j, v);
-	v.menu_items.push({title: "Own menu item",
-			   visible: function() {
-			       return true;
-                           },
-			   action: function() {
-			       R.Playlists.showAddToPlaylistDialog(j);
-			       return false;
-			   }});
+    jQuery.fn.autoSuspenders = function(data, item) {
+	var result = jQuery.fn.origAutoSuspenders.call(this, data, item);
+	if (item) {
+	    console.log(item);
+	    item.menu_items.splice(7, 0,
+				   {title: "Add Album to Playlist",
+				    visible: function() {
+					return data.key.indexOf('a') == 0;
+				    },
+				    action: function() {
+					var copy = jQuery.extend(true, {}, data);
+					copy.key = data.trackKeys;
+					R.Playlists.showAddToPlaylistDialog(copy);
+					return false;
+				    }});
+	}
 	return result;
+    }
+    R.Api.origRequest = R.Api.request;
+    R.Api.request = function() {
+	console.log(arguments);
+	var args = arguments[0];
+	if (args.method == 'addToPlaylist') {
+	    var tracks = args.content.tracks;
+	    if (tracks.length == 1 && tracks[0] instanceof Array) {
+		console.log("hey I'm an album", tracks[0]);
+		R.Api.request({method: "addToPlaylist", 
+			       content: { playlist: args.content.playlist, 
+					  tracks: tracks[0] }, 
+			       success: function(arg) { 
+				   console.log("success", arg); 
+			       }});
+		return;
+	    }
+	}
+	return R.Api.origRequest.apply(this, arguments);
     }
 }
 
